5736f46ab52546d293e3d059b2c770a53437df62
Ankan Sikdar <ankansikdar@gmail.com>
2019-09-18 21:15:42 +0530

    Finished API

---
+++ controllers/image.js
@@ -0,0 +1,24 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({apiKey: "68015a16c0f049dabc81bbc929222ccf"});
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body
+    db("users").where("id", "=", id)
+        .increment("entries", 1)
+        .returning("entries")
+        .then(entry => res.json(entry))
+        .catch(err => res.status(400).json("Unable to get entries"))
+}
+
+const handleApiCall = (req, res) => {
+    const {input} = req.body
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
+        .then(response => res.json(response))
+        .catch(err => res.status(400).json(err))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
---
+++ controllers/profile.js
@@ -0,0 +1,18 @@
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params
+    db('users').select("*")
+        .where({
+            id: id
+        })
+        .then(user => {
+            if(user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json("Error getting user")
+            }
+        })
+}
+
+module.exports = {
+    handleProfileGet: handleProfileGet
+}
